bebb5ad8aa7e0f04f35774bdfb8e358649fcd46d
xena <74474948+a-andres1@users.noreply.github.com>
2021-04-27 18:21:56 -0400

    adding Mongodb uri

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const uri = process.env.MONGODB_URI;
+
 
 const PORT = 3000;
 
